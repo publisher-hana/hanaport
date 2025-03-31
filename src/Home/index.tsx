b748422a0c7f1be91fda213d8514da635f82606f
unknown <lhn1208@naver.com>
2025-03-31 19:39:42 +0900

    아이콘추가

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -46,7 +46,7 @@ export const Home = () => {
             </div>
             <ToMain>
                 <Link to="/portfolio">
-                    <span className="txt">메인으로 가기</span>
+                    <span className="txt">Go to main</span>
                     <span className="line1"></span><span className="line2"></span><span className="line3"></span><span className="line4"></span>
                 </Link>
                 <span className="desc">Made with React</span>
